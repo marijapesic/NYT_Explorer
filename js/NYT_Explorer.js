520c820cbe3c223b7ff19debee43a751fe4d04c6
marijapesic <marija_cka@yahoo.com>
2018-03-03 16:52:56 +0100

    done request and response to articles details, done css

--- js/NYT_Explorer.js
+++ js/NYT_Explorer.js
@@ -1,131 +1,136 @@
-class App extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      urls:[],
-      urlDetails:{},
-    };
-    this.setData = this.setData.bind(this);
-    this.urlClick=this.urlClick.bind(this);
-  }
-  setData(responseData){
-    let urls = [];
-    for(var i =0;i<2;i++){
-      const url = responseData.response.docs[i];
-      // urls.push(doc.web_url);
-      urls.push(url);
+// defining component that receives API response
+class App extends React.Component {
+    constructor(props) {
+
+        console.log('App: constructor')
+
+        super(props)
+        this.state = {
+            urls: [],
+        }
+        this.setData = this.setData.bind(this);
     }
-    this.setState({urls:urls});
-    console.log(urls[0]);
-  }
-  urlClick(data){
-    console.log(data);
-    this.setState({urlDetails:{
-      "headline":data.headline.main,
-      "publish date":data.pub_date,
-      "description":data.abstract,
-      "link":data.web_url
+
+    setData(responseData) {
+        let urls = []
+        for (var i = 0; i < 20; i++) {
+            const url = responseData.response.docs[i];
+            urls.push(url);
+        }
+        this.setState({ urls: urls });
+        //console.log(articles[0])
     }
-    })
-    alert(JSON.stringify(this.state.urlDetails,null,3));
-  }
-  componentDidMount(){
-    const month =document.getElementById('month').value;
-    const year = document.getElementById('year').value;
-    const url='https://api.nytimes.com/svc/archive/v1/'+year+'/'+month+'.json';
-    $.ajax({
-    // const month =document.getElementById('month'),
-    // const year = document.getElementById('year'), 
-    // url: 'https://api.nytimes.com/svc/archive/v1/'+year+'/'+month+'.json',
-     url:url,
-     method: 'GET',
-     data:{
-     'api-key': "10aa395d2cea4878a49407b6f5c244cf"
-     },
-     success:this.setData
-    });
-  }
-render(){
-  if (!this.state.urls || this.state.urls.length == 0) {
-      return 'Waiting for results...';
-  }
-  return (<Urls urlClick={this.urlClick} urls={this.state.urls} />)
-}
-}
 
-class UrlTable extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {jsondata:{
-    "title":"Tittle not available-click here for details",
-    "description":"Description not available-click here for details",
-    "image":"../img/simbol picture.jpg",
-    "url":"https:\/\/www.google.com\/"
+    componentDidMount() {
+        // console.log('App: componentDidMount');
+        const month = document.getElementById('month').value;
+        const year = document.getElementById('year').value;
+        const url = 'https://api.nytimes.com/svc/archive/v1/' + year + '/' + month + '.json';
+        $.ajax({
+            url: url,
+            method: 'GET',
+            data: {
+                'api-key': "10aa395d2cea4878a49407b6f5c244cf"
+            },
+            success: this.setData
+        });
     }
-    }
-    this.secondApiResponse = this.secondApiResponse.bind(this);
-    this.urlClick=this.urlClick.bind(this);
 
-  }
-secondApiResponse(response){
-  console.log(response);
-  this.setState({jsondata:response});
-}
-urlClick(){
-  this.props.urlClick(this.props.url);
-}
-
-componentDidMount(){
-  const linkUrl='http://api.linkpreview.net/?key=5a9154e86692930fecbdcc460ef0bc018ebb6057ebc84&q='+this.props.url.web_url;
-  $.ajax({
-    url:linkUrl,
-    method:'GET',
-    success: this.secondApiResponse
-  });
-}
-render(){
-  return(
-    <table className = 'linkData' onClick={this.urlClick}>
-      <tbody>
-      <tr><td>{this.state.jsondata.title}</td></tr>
-      <tr><td>{this.state.jsondata.description}</td></tr>
-      <tr><td><img src = {this.state.jsondata.image}/></td></tr>
-      </tbody>
-    </table>
-  );
-}
-}
-
-
-class Urls extends React.Component{
-  render(){
-    return(
-      <div id='urls'>
-      {this.props.urls.map((url,index)=><UrlTable urlClick={this.props.urlClick} key={index} url={url}/>
+    render() {
+        // console.log('App: render');
+        if (!this.state.urls || this.state.urls.length == 0) {
+            return 'Checking of results...';
+        }
+        return ( <
+            div id = "articles" > {
+                this.state.urls.map(
+                    (url, index) => < ArticleTable key = { index } url = { url } id = { index }
+                    />
+                )
+            } <
+            /div>
         )
     }
-    </div>
-    )
-  }
 }
 
-// function Urls(props){
-//   return (
-//   <div id = "articles">
-//   {this.props.urls.map(
-//     (url,index)=> <Url urlsClick={this.urlsClick} url={this.state.urls}/>
-//     )
-//   }
-//   </div>
-//   );
-// }
-// function Url(props){
-//   return <p><a href ={props.url} target = "_blank"> {props.url}</a></p>;
-// }
+//defining component that gets and renders article/articles info 
+class ArticleTable extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            jsondata: {
+                "title": "Tittle not available",
+                "description": "Description not available",
+                "image": "./img/backgrund1a.jpg",
+            },
+            urlDetails: {
+                "headline": this.props.url.headline.main,
+                "publish date": this.props.url.pub_date,
+                "topic": this.props.url.section_name,
+                "link": this.props.url.web_url
+            },
+        }
 
-function search(){
-  const root = document.getElementById('root');
-  ReactDOM.render(<App />,  root);
- }
+        this.setData = this.setData.bind(this);
+        this.urlClick = this.urlClick.bind(this);
+    }
 
+    setData(response) {
+        //console.log(response)
+        this.setState({ jsondata: response });
+    }
 
+    urlClick() {
+        var details = $("#" + this.props.id);
+        details.toggle();
+    }
+
+    componentDidMount() {
+        const url = 'https://api.linkpreview.net/?key=5a9154e86692930fecbdcc460ef0bc018ebb6057ebc84&q=' + this.props.url.web_url
+        $.ajax({
+            url: url,
+            method: 'GET',
+            success: this.setData
+        });
+    }
+    //jsx part of code which is rendering as table 
+    render() {
+        return ( 
+          <table className = 'article' >
+            <tbody className = "articleInfo" >
+              <tr><th> { 'Title: ' + this.state.jsondata.title } </th></tr>
+              <tr><td className = "description"> { 'Description: ' + this.state.jsondata.description } </td></tr>
+              <tr><td><img src = { this.state.jsondata.image }/></td></tr>
+              <tr><td className = "click" onClick = { this.urlClick }> { 'Click for details.' } </td></tr>
+            </tbody> 
+            <tbody >
+              <tr><td className = "details" id = { this.props.id }><p> { 'Details:' + JSON.stringify(this.state.urlDetails, null, 3) } </p></td></tr>
+            </tbody>
+          </table>
+        );
+    }
+}
+
+//function for validation of input
+function yearSelected() {
+    const y = document.getElementById('year').value;
+    const m = document.getElementById('month');
+    if (y === '1851') {
+        m.min = "9";
+    } else if (y === '2018') {
+        m.max = "3";
+    } else {
+        m.min = "1";
+        m.max = "12";
+    }
+}
+//function that is called on click "FIND"
+function search(event) {
+
+    console.log('search ...')
+
+    event.preventDefault();
+    const root = document.getElementById('root');
+    ReactDOM.unmountComponentAtNode(root);
+    ReactDOM.render( <App /> , root);
+}
